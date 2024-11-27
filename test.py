6e0061d601dc7e20941f4946b30d16cd95774d91
PHT373 <168071323+PHT35353@users.noreply.github.com>
2024-11-27 23:58:19 +0100

    Update test.py

--- test.py
+++ test.py
@@ -503,13 +503,11 @@ function sendPipeDataToBackend() {{
         const endCoord = feature.geometry.coordinates[feature.geometry.coordinates.length - 1];
 
         // Identify landmarks for the start and end points of the line
-        const startLandmark = landmarks.find(lm => turf.distance(lm.geometry.coordinates, startCoord) < 0.01);
-        const endLandmark = landmarks.find(lm => turf.distance(lm.geometry.coordinates, endCoord) < 0.01);
+        let startLandmark = landmarks.find(lm => turf.distance(lm.geometry.coordinates, startCoord) < 0.01);
+        let endLandmark = landmarks.find(lm => turf.distance(lm.geometry.coordinates, endCoord) < 0.01);
 
         // Format the pipe name to include landmark names
-        const pipeName = `Line ${{pipeData[pipeId].name}} belongs to ${{
-            startLandmark?.properties.name || 'Unknown'
-        }} - ${{endLandmark?.properties.name || 'Unknown'}}`;
+        const pipeName = `Line ${{pipeData[pipeId].name}}`;
 
         return {{
             name: pipeName, // Use the formatted name
